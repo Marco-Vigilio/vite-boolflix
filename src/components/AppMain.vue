536e374cd19ca447f8f5be71d621f3e5c075aee3
Marco-Vigilio <tonyvigilio@gmail.com>
2023-06-21 19:12:04 +0200

    inserisco bandiere

--- src/components/AppMain.vue
+++ src/components/AppMain.vue
@@ -13,7 +13,8 @@ export default {
     name: "AppMain",
     data() {
         return {
-            url: "https://api.themoviedb.org/3/search/movie?api_key=514cb42a7e6e0b17e72add7954487401",
+            urlFilm: "https://api.themoviedb.org/3/search/movie?api_key=514cb42a7e6e0b17e72add7954487401",
+            urlSerieTV: "https://api.themoviedb.org/3/search/tv?api_key=514cb42a7e6e0b17e72add7954487401",
             store,
         }
     },
@@ -22,26 +23,32 @@ export default {
         ViewItems,
     },
     methods: {
-        apiObject(genericUrl) {
+        apiObjectFilm(genericUrl) {
             axios.get(genericUrl)
                 .then(risposta => {
                     this.store.arrayObject = [];
                     risposta.data.results.forEach(element => {
                         this.store.arrayObject.push(element);
-                        console.log(element);
+                        //console.log(element);
                     });
+                });
+        },
+
+        apiObjectSerieTV(genericUrl) {
+            axios.get(genericUrl)
+                .then(risposta => {
+                    console.log(risposta);
                 });
         },
 
         wordOfMouth(word) {
             if (word === "") {
-                this.apiObject(this.url);
+                this.apiObjectFilm(this.urlFilm);
             }
             else {
-                console.log(word);
-                let newUrl = this.url + "&query=" + word;
-                console.log(newUrl);
-                this.apiObject(newUrl);
+                let newUrl = this.urlFilm + "&query=" + word;
+                this.apiObjectFilm(newUrl);
+                this.apiObjectSerieTV(newUrl)
             }
 
         },
